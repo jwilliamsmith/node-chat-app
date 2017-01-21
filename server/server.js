2ad14c322bf72ff135f4bcf0fecba1554042571f
jwilliamsmith <perfiction@gmail.com>
2017-01-21 11:51:16 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+'use strict';
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const express = require('express');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+	console.log(`Server is listening on port ${port}`);
+});
+
